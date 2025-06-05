53d58ebfc0f413ef2cc9e23dcffffa255489e702
samuelhattonCU <samuel.hatton@colorado.edu>
2025-06-05 13:01:44 -0600

    Update to reflect name change, expanded documentation

--- example_main.py
+++ example_main.py
@@ -4,7 +4,7 @@ This script contains an example workflow for ingesting and analyzing HDF5 data f
 '''
 
 # Import our custom functions:
-import functions as f
+import mpe_h5_explorer_tool as met
 
 # Import matplotlib for plotting:
 import matplotlib.pyplot as plt
@@ -19,21 +19,21 @@ output_file_path = '/media/sam/METALS extl/DATA/METALS/Fatigue/MTS/Sample 27/Spe
 
 # Quickly check that the databases are in a valid format:
 
-# f.check_h5_file(low_freq_cycles_path, output_file="low_freq_h5_check.txt")
-# f.check_h5_file(high_freq_cycles_path, output_file="high_freq_h5_check.txt")
+# met.check_h5_file(low_freq_cycles_path, output_file="low_freq_h5_check.txt")
+# met.check_h5_file(high_freq_cycles_path, output_file="high_freq_h5_check.txt")
 #     # NOTE: If these two text files contain similar outputs, and you can `ctrl+f` search for a field, like "Running Time" and find it, that's a good sign that the datafiles are correctly formatted.
 
 
 
 # # Combine databases into one file and sort by running time:
 
-# f.combine_and_sort_h5_files([low_freq_cycles_path, high_freq_cycles_path], output_file_path)
+# met.combine_and_sort_h5_files([low_freq_cycles_path, high_freq_cycles_path], output_file_path)
 
 
 # # Extract and print the avialiable signals in the combined file:
 
 # print("\nChecking signals in the combined file:")
-# signal_names, signal_units = f.extract_and_print_signal_info(output_file_path)
+# signal_names, signal_units = met.extract_and_print_signal_info(output_file_path)
 
 # if signal_names and signal_units:
 #     print("Available signal names:", signal_names)
@@ -43,7 +43,7 @@ output_file_path = '/media/sam/METALS extl/DATA/METALS/Fatigue/MTS/Sample 27/Spe
 # Make a basic plot of force vs. time:
 
 # Use `get_h5_plot_stuff` to extract data columns from the database:
-x_data, y_data, x_label, y_label, title_str, x_unit, y_unit = f.get_h5_plot_stuff(output_file_path, x_signal_name=b"Running Time", y_signal_name=b"Axial Force")
+x_data, y_data, x_label, y_label, title_str, x_unit, y_unit = met.get_h5_plot_stuff(output_file_path, x_signal_name=b"Running Time", y_signal_name=b"Axial Force")
 
 # set figure save path:
 output_plot_path = '/media/sam/METALS extl/DATA/METALS/Fatigue/MTS/Sample 27/Specimen 1/maybe 10mil h5/axial_force_vs_time.png'
